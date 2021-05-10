b6d1b0a7d001522ad1d53d62023c693b9c838b90
Dantz Farrow <dantzfarrow@gmail.com>
2021-05-10 02:32:28 -0700

    Add project files.

---
+++ CSV.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSVParsing
+{
+    class CSV
+    {
+        public static List<String> getCSV(string input)
+        {
+            List<string> output = new List<string>();
+            string temp = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i] == '\"')
+                {
+                    i++;
+                    while (input[i] != '\"')
+                    {
+                        temp += input[i];
+                        i++;
+                    }
+                }
+
+                if (input[i] != '\"' && input[i] != ',')
+                {
+                    temp += input[i];
+                }
+
+                if (input[i] == ',' || input[i] == '\n' || i == input.Length - 1)
+                {
+                    output.Add(temp);
+                    temp = "";
+                }
+            }
+            return output;
+        }
+    }
+}
+//Reference code respository: https://github.com/Makairo/CSVParsing
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSVParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /************************** 
+             * read CSV with embedded commas
+             * parse CSV into separate fields and
+             * ignore commas within quoted string
+             * ***********************/
+            Console.WriteLine("Reading CSV with embedded commas");
+            List<string> myList = new List<string>();
+            string input1 = "\"a,b\",c";
+            myList.Add(input1);
+            string input2 = "\"Obama, Barack\",\"August 4, 1961\",\"Washington, D.C.\"";
+            myList.Add(input2);
+            string input3 = "\"Ft. Benning, Georgia\",32.3632N,84.9493W," + "\"Ft. Stewart, Georgia\",31.8691N,81.6090W," + "\"Ft. Gordon, Georgia\",33.4302N,82.1267W";
+            myList.Add(input3);
+            foreach (string s in myList)
+            {
+                Console.WriteLine($"Current input is {s}");
+                List<string> output = CSV.getCSV(s);
+                int len = output.Count;
+                Console.WriteLine($"This line has {len} fields. They are:");
+                foreach (string s1 in output)
+                    Console.WriteLine(s1);
+            }
+        }
+    }
+}
